4e0342014286f9e2eab7cd223c2c0a203ddcdd7b
tgeorgy <georgtrof@gmail.com>
2016-05-27 20:12:02 +0600

    added : torch interface

--- environment/python2-cgdk/MyStrategy.py
+++ environment/python2-cgdk/MyStrategy.py
@@ -3,6 +3,8 @@ from model.Game import Game
 from model.Move import Move
 from model.World import World
 
+import numpy
+import requests
 
 class MyStrategy:
     def move(self, me, world, game, move):
@@ -12,9 +14,14 @@ class MyStrategy:
         @type game: Game
         @type move: Move
         """
+        
+        resp = requests.post('http://127.0.0.1:5010',data='test=1').json()
+        move.engine_power = resp['engine']
+        """
         move.engine_power = 1.0
         move.throw_projectile = True
         move.spill_oil = True
 
         if world.tick > game.initial_freeze_duration_ticks:
             move.use_nitro = True
+        """
