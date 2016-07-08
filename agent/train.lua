03f52540d78198e2d787d7e0e94294286099a121
tgeorgy <georgtrof@gmail.com>
2016-07-08 17:50:47 +0600

    changed : agent init

--- agent/train.lua
+++ agent/train.lua
@@ -1,9 +1,15 @@
+torch.setdefaulttensortype('torch.FloatTensor')
 local app = require('waffle')
 require 'json'
+require 'util'
+require 'Agent'
+
+agent.Agent()
 
 app.post('/', function(req, res)
-    print(#req.body)
-    res.json({action=6})
+    t,r,s = process_input(#req.body)
+    action = agent.train_iter(t,r,s)
+    res.json({action=action})
 end)
 
 app.listen({port=5010})
